9998e5e7410471b641eb9194223eef82c4b8493c
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-09-08 12:32:17 +0530

    adding routing to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./Routes/RootLoyout";
+import NewPost from "./components/NewPost";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/new-post", element: <NewPost /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
